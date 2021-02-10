75f702e6eebbac2512d5f276de1ea9999fbd2f4b
ddikodroid <ddikodroid@gmail.com>
2021-02-10 11:53:49 +0700

    button component

---
+++ src/components/CharacterButton.js
@@ -0,0 +1,39 @@
+
+import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { COLOR } from '../styles/Color'
+import { FONT, WIDTH } from '../styles/Dimension'
+
+const CharacterButton = ({ character, keys, onPress }) => {
+  return (
+    <TouchableOpacity
+      key={keys}
+      disabled={character === ' ' ? true : null}
+      style={styles.buttonContainer}
+      onPress={onPress}
+    >
+      <Text
+        style={{ ...FONT.h1, color: (typeof (character) === 'string' ? COLOR.teal : COLOR.white) }}
+      >
+        {character}
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
+export default CharacterButton
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    width: WIDTH / 5,
+    height: WIDTH / 5,
+    borderRadius: WIDTH / 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: COLOR.notTooDark,
+    margin: 5
+  },
+  buttonFont: {
+    ...FONT.h1
+  }
+})
