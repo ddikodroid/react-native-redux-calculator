86b986133daf4b0548b8c97023454ea656906926
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2021-09-17 02:07:49 +0700

    pass accessibilityLabel prop to TouchableOpacity component

--- src/components/CharacterButton.js
+++ src/components/CharacterButton.js
@@ -1,30 +1,40 @@
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+import {COLOR} from '../styles/Color';
+import {FONT, WIDTH} from '../styles/Dimension';
 
-import React from 'react'
-import { StyleSheet, Text, TouchableOpacity } from 'react-native'
-import { COLOR } from '../styles/Color'
-import { FONT, WIDTH } from '../styles/Dimension'
-
-const CharacterButton = ({ character, keys, onPress }) => {
+const CharacterButton = ({character, keys, onPress, accessibilityLabel}) => {
   const onClick = () => {
-    onPress(character)
-  }
+    onPress(character);
+  };
   return (
     <TouchableOpacity
       key={keys}
       disabled={character === ' ' || character === '' ? true : null}
       style={styles.buttonContainer}
       onPress={onClick}
-    >
+      accessibilityLabel={accessibilityLabel}>
       <Text
-        style={[FONT.h1, { color: typeof (character) === 'number' ? COLOR.white : (character === '00' || character === '000' ? COLOR.white : (character === 'AC' || character === 'DEL' ? COLOR.red : COLOR.teal)) }]}
-      >
+        style={[
+          FONT.h1,
+          {
+            color:
+              typeof character === 'number'
+                ? COLOR.white
+                : character === '00' || character === '000'
+                ? COLOR.white
+                : character === 'AC' || character === 'DEL'
+                ? COLOR.red
+                : COLOR.teal,
+          },
+        ]}>
         {character}
       </Text>
     </TouchableOpacity>
-  )
-}
+  );
+};
 
-export default CharacterButton
+export default CharacterButton;
 
 const styles = StyleSheet.create({
   buttonContainer: {
@@ -34,9 +44,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: COLOR.notTooDark,
-    margin: 5
+    margin: 5,
   },
   buttonFont: {
-    ...FONT.h1
-  }
-})
+    ...FONT.h1,
+  },
+});
